f1d5c26301e32db96341a004079bc327192521c3
Shana Wiltshire <shanawiltshire@gmail.com>
2019-10-26 20:31:39 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -2,13 +2,9 @@ def addition(num1, num2)
   puts "#{num1} + #{num2}"
 end
 
-addition (5,6)
-
 def subtraction(num1, num2)
 puts "#{num1} - #{num2}"
 end
-
-subtraction("10,5")
 
 def division(num1, num2)
 
